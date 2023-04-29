93bb09c399abeeba19cb00e8773d2a8a6f982070
migusdn <migusdn@gmail.com>
2023-04-30 03:17:39 +0900

    Create Xcode Project and Configuring Project Structure

---
+++ frontend/Atinarae/Atinarae/AtinaraeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtinaraeApp.swift
+//  Atinarae
+//
+//  Created by HyunwooPark on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct AtinaraeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
