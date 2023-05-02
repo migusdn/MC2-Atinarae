408f98355c5576119d4126c8e1a247b0060f7226
migusdn <migusdn@gmail.com>
2023-05-02 20:50:22 +0900

    Create Models

---
+++ frontend/Atinarae/Atinarae/models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Atinarae
+//
+//  Created by HyunwooPark on 2023/05/02.
+//
+
+import Foundation
+
+class User {
+    let userId:Int
+    let userNickname:String
+    let userMail: String
+    let userPhone: String
+    init(userId: Int, userNickname: String, userMail: String, userPhone: String, userCategories: [String], receiverList: [VideoMessage]) {
+        self.userId = userId
+        self.userNickname = userNickname
+        self.userMail = userMail
+        self.userPhone = userPhone
+    }
+    
+}
