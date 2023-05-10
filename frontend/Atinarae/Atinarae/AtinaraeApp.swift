40a74ce025fa282afeabb879561ec77cbea2ccf8
migusdn <migusdn@gmail.com>
2023-05-10 15:44:04 +0900

    [Inbox] view완성

--- frontend/Atinarae/Atinarae/AtinaraeApp.swift
+++ frontend/Atinarae/Atinarae/AtinaraeApp.swift
@@ -14,13 +14,20 @@
 import SwiftUI
 
 @main
-struct AtinaraeApp: App {
-    let appData = AppData()
-    var body: some Scene {
-        WindowGroup {
-            LunchPage()
-//            VideoSettingsView()
-                .environmentObject(appData)
-        }
+//struct AtinaraeApp: App {
+//    let appData = AppData()
+//    var body: some Scene {
+//        WindowGroup {
+//            LunchPage()
+////            VideoSettingsView()
+//                .environmentObject(appData)
+//        }
+//    }
+//}
+struct testConsoleApp{
+    static func main(){
+        let appData = AppData();
+        
+        print(appData.getPeopleFromVideos(videos: appData.user.inbox.videos))
     }
 }
