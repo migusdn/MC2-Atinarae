063ba4c891b076540636ccb57afe5e97f01bb2f9
migusdn <migusdn@gmail.com>
2023-05-10 16:08:05 +0900

    [FIX] conflict

--- frontend/Atinarae/Atinarae/views/VideoSetting/VideoSettingsView.swift
+++ frontend/Atinarae/Atinarae/views/VideoSetting/VideoSettingsView.swift
@@ -144,7 +144,7 @@ struct VideoSettingsView: View {
         VStack(alignment: .center) {
             if selectedFriend == friend {
                 ZStack{
-                    Image(friend.planetImage)
+                    Image(friend.planetImage!)
                         .resizable()
                         .frame(width: 52, height: 52)
                     Circle()
@@ -154,13 +154,13 @@ struct VideoSettingsView: View {
                     
                     
                 }
-                Text(friend.nickname)
+                Text(friend.nickname!)
             } else {
-                Image(friend.planetImage)
+                Image(friend.planetImage!)
                     .resizable()
                     .foregroundColor(Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1)))
                     .frame(width: 63, height: 63)
-                Text(friend.nickname)
+                Text(friend.nickname!)
             }
         }
         .padding(.top, 2)
